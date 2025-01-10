dbc8d56e459572fa9712102e71066910dd19bc9f
Ajay kumar <akjajay9315@gmail.com>
2025-01-10 11:06:50 +0530

    secret2

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -33,36 +33,36 @@
 // export { handler as GET, handler as POST }
 
 
-import NextAuth from "next-auth/next";
+  import NextAuth from "next-auth/next";
 
-export const authOptions = {
-  providers: [
-    {
-      id: "descope",
-      name: "Descope",
-      type: "oauth",
-      wellKnown: `https://api.descope.com/`+process.env.DESCOPE_API+`/.well-known/openid-configuration`,
-      // wellKnown: `https://api.descope.com/P2rLHSgulXpeelzQKToJxqLue5Vr/.well-known/openid-configuration`,
-      authorization: { params: { scope: "openid email profile" } },
-      idToken: true,
-      clientId: process.env.DESCOPE_CLIENT_ID,
-      // clientId: "P2rLHSgulXpeelzQKToJxqLue5Vr",
-      // clientSecret: "<Descope Access Key>",
-      clientSecret: process.env.DESCOPE_CLIENT_SECRET,
+  export const authOptions = {
+    providers: [
+      {
+        id: "descope",
+        name: "Descope",
+        type: "oauth",
+        wellKnown: `https://api.descope.com/`+process.env.DESCOPE_API+`/.well-known/openid-configuration`,
+        // wellKnown: `https://api.descope.com/P2rLHSgulXpeelzQKToJxqLue5Vr/.well-known/openid-configuration`,
+        authorization: { params: { scope: "openid email profile" } },
+        idToken: true,
+        clientId: process.env.DESCOPE_CLIENT_ID,
+        // clientId: "P2rLHSgulXpeelzQKToJxqLue5Vr",
+        // clientSecret: "<Descope Access Key>",
+        clientSecret: process.env.DESCOPE_CLIENT_SECRET,
 
-      checks: ["pkce", "state"],
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-        };
+        checks: ["pkce", "state"],
+        profile(profile) {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            email: profile.email,
+            image: profile.picture,
+          };
+        },
       },
-    },
-  ],
-};
+    ],
+  };
 
-const handler = NextAuth(authOptions);
+  const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+  export { handler as GET, handler as POST };
