16e5ed8a1b134e21c0db2314cf3c006219a5249c
Ajay kumar <akjajay9315@gmail.com>
2025-01-10 13:38:46 +0530

    search

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -8,16 +8,16 @@ function Hero() {
     <div className='flex items-center gap-3 flex-col justify-center pt-14 pb-7'>
         <h2 className='font-bold text-[46px] text-center'>
             Find Home 
-            <span className='text-primary'> Service/Repair</span>
+            <span className='text-primary'> Help/Service</span>
             <br></br> Near You</h2>
-        <h2 className='text-xl text-gray-400'>Explore Best Home Service & Repair near you</h2>
-        <div className='mt-4 flex gap-4 items-center'>
+        <h2 className='text-xl text-gray-400'>Explore Best Home Help & Service near you</h2>
+        {/* <div className='mt-4 flex gap-4 items-center'>
             <Input placeholder='Search'
             className="rounded-full md:w-[350px]" />
             <Button className="rounded-full h-[46px]">
                 <Search className='h-4 w-4'/>
             </Button>
-        </div>
+        </div> */}
     </div>
   )
 }
